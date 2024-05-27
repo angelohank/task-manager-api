3958a71fee8035629d75cee75108e5194130a7ad
luizhs331 <luizexhs@gmail.com.br>
2024-05-26 22:18:57 -0300

    Modificações efetuadas em: task.js

--- models/task.js
+++ models/task.js
@@ -43,7 +43,6 @@ module.exports = (sequelize, DataTypes) => {
       sequelize,
       modelName: "Task",
       tableName: "tasks",
-      freezeTableName: true,
       timestamps: false,
     }
   );
