2538d960c68366a8bc1539b48314cd53b32dcac9
luizhs331 <luizexhs@gmail.com.br>
2024-06-27 20:04:06 -0300

    feat: Adicionar rota do swagger

--- server.js
+++ server.js
@@ -3,11 +3,14 @@ require("module-alias/register");
 const express = require("express");
 const Router = require("@root/routes/Router");
 const cors = require("cors");
+const SwaggerDocument = require("@root/config/swagger-doc");
+const SwaggerUI = require("swagger-ui-express");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use("/api/docs", SwaggerUI.serve, SwaggerUI.setup(SwaggerDocument));
 
 app.use("/api", Router);
 
